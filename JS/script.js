53ae4f71f02461f0654ea3b4eca65b7828e59653
junior <lantoniodasilvaj@gmail.com>
2023-08-28 23:52:45 -0300

    secound cpmmit

--- JS/script.js
+++ JS/script.js
@@ -1,33 +1,63 @@
+const projectors = ["001", "002", "003", "004", "005"];
+const classrooms = [1,2,3,4,5,6,7,8,9,10,11,"biblioteca", "laboratório"];
+
+const reservationArray = [];
+
 const form = document.querySelector("#form");
-const reserveName = document.querySelector("#reserveName");
-const reserveDate = document.querySelector("#reserveDate");
-const reserveHourInitial = document.querySelector("#reserveHourInitial"); 
-const reserveHourFinal = document.querySelector("#reserveHourFinal");
+const reservationForName = document.querySelector("#reservationName");
+const bookingDate = document.querySelector("#bookingDate");
+const bookingStartTime = document.querySelector("#bookingStartTime"); 
+const bookingEndTime = document.querySelector("#bookingEndTime");
 const reserveProjector = document.querySelector("#reserveProjector");
+const reserveLocation = document.querySelector("#reserveLocation");
 const btnAddReserve = document.querySelector("#btnAddReserve");
 const reservationList = document.querySelector("#reservationList");
 
 const openForm = document.querySelector("#openForm");
 const closeForm = document.querySelector("#closeForm");
 
+function showProjectors(projectors){
+  projectors.forEach((option) => {
+    const createOption = document.createElement("option");
+    createOption.innerText = option;
+    reserveProjector.appendChild(createOption);
+  });
+};
+showProjectors(projectors);
+
+function showClassrooms(room){
+  room.forEach((option) => {
+    const createOption = document.createElement("option");
+    createOption.innerText = option;
+    reserveLocation.appendChild(createOption);
+  });
+};
+showClassrooms(classrooms);
+
 function showForm(){
-  form.classList.add("nada")
-}
-
+  form.classList.remove("hidden");
+  form.classList.add("on");
+} 
 function noShowForm(){
-  form.classList.add("hidden")
+  form.classList.remove("on");
+  form.classList.add("hidden");
 }
-console.log(form)
 
-function createBookingCard(name, date, initialHour, finalHour, projector){
+openForm.addEventListener("click", ()=>{
+  showForm()
+});
+closeForm.addEventListener("click", ()=>{
+  noShowForm()
+});
+
+function createBookingCard(name, date, initialHour, finalHour, projector, room){
   const liCard = document.createElement("div");
-  
   liCard.innerHTML = `
-  <p>${name}</p>
-  <p>${date}</p>
-  <p>${initialHour}</p>
-  <p>${finalHour}</p>
-  <p>${projector}</p>
+  <p>Professor: ${name}</p>
+  <p>Data: ${date}</p>
+  <p>De: ${initialHour} até: ${finalHour}</p>
+  <p>Referência do projetor: ${projector}</p>
+  <p>Referência da localização: ${room}</p>
   `;
 
   reservationList.appendChild(liCard);
@@ -37,11 +67,12 @@ function createBookingCard(name, date, initialHour, finalHour, projector){
 const start = () => {
   event.preventDefault();
   createBookingCard(
-    reserveName.value,
-    reserveDate.value,
-    reserveHourInitial.value,
-    reserveHourFinal.value,
-    reserveProjector.value
+    reservationForName.value,
+    bookingDate.value,
+    bookingStartTime.value,
+    bookingEndTime.value,
+    reserveProjector.value,
+    reserveLocation.value
   )
 };
 
